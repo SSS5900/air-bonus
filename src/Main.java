b92cfc3d470c30e5071f656c73f3135065917107
SSS5900 <sosnovskihsergey@gmail.com>
2023-03-08 23:48:33 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 24365;
+        int pricemile = 10;
+
+        int mile = price / pricemile;
+
+        System.out.println("Начислено " + mile + " бонусных миль");
+
+    }
+}
